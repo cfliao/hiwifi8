bc71bebd0a70d5d92bcc4712e00f56d14042482f
jsong@hiwifi.net <jsong@hiwifi.net@9d751782-d38d-8aa7-4df3-7b238aaa7bf0>
2011-12-15 07:24:45 +0000

    cwmp session support

--- trunk/cwmp/acsngin/session.h
+++ trunk/cwmp/acsngin/session.h
@@ -1,6 +1,15 @@
 #ifndef _SESSION_H_
 #define _SESSION_H_
 
+inline void init_session_env();
+inline const char * get_cookie_domain();
+inline const char * get_cookie_path();
+inline const int get_cookie_expire();
 
+void new_session_id(char * sessionid, int strength);
+
+int session2database(struct soap* soap, const char *sessionid);
+int chk_session_valid(struct soap* soap, const char *sessionid);
+int chk_session_valid(struct soap* soap, const char *sessionid);
 
 #endif /* ~_SESSION_H_ */
